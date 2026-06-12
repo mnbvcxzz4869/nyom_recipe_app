1eb220121cf16b08d440d86ec5b08bbae120e154
mnbvcxzz4869 <94119615+mnbvcxzz4869@users.noreply.github.com>
2026-06-13 03:57:26 +0700

    remove unused var

--- lib/features/auth/screens/reset_screen.dart
+++ lib/features/auth/screens/reset_screen.dart
@@ -61,7 +61,6 @@ class _ResetPasswordScreenState extends ConsumerState<ResetPasswordScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
     return Stack(
       children: [
         Scaffold(
